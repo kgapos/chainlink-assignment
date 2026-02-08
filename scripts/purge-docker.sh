3d6b80763a1b40af20d73a8c97ada871a9284631
Kostas Apostolopoulos <kgapos@outlook.com>
2026-02-08 17:24:53 +0000

    Update bash scripts for consistency

---
+++ scripts/purge-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -eu
+
+cat << 'EOF'
+Purpose: Purges all docker networks, containers, images and volumes.
+
+|-------------------|
+| Use with caution! |
+|-------------------|
+This deletes all docker networks, containers, images and volumes, not just the ones created by this project.
+EOF
+
+
+read -p "Are you sure you want to purge all docker networks, containers, images and volumes? (y/n) " answer
+if [ "$answer" != "y" ]; then
+  echo "Aborting..."
+  exit 1
+fi
+
+docker network prune -f
+docker container prune -f
+docker image prune -af
+docker volume prune -f
+
+echo "Docker purged."
